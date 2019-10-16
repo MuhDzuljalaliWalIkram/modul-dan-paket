b6f0f52ddcba7c85d0d05f63b4c710b500c1278b
MuhDzuljalaliWalIkram <48996414+MuhDzuljalaliWalIkram@users.noreply.github.com>
2019-10-16 19:47:01 +0800

    Create paket1.py

---
+++ paket1.py
@@ -0,0 +1,13 @@
+from Matematika.geometri2D import luasBujursangkar
+
+def main():
+  # bujursangkar
+  sisi = 5
+  
+  luas = luasBujursangkar(sisi)
+  
+  print("BUJURSANGKAR")
+  print("Panjang sisi\t: ", sisi)
+  
+if __name__ == "__main__":
+  main()
